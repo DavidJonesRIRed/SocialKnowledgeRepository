a6edac46cf76256089b7e1c9222e810a9b8a80c5
David Jones <coreldavid@hotmail.com>
2020-04-12 17:13:31 -0400

    Added resource

---
+++ src/components/Resource.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Resource = ({resource}) => {
+    
+    return(
+        <div className={'box'}>
+            <h2>{ resource.title } </h2>
+            <p>{ resource.author }</p>
+            <p>Published on: { resource.datePublished }</p>
+            { resource.videoLength ? ( <p>{ resource.videoLength }</p> ) : null }
+            { /* conditional / true code : false code */ }
+            <p>Comments: { resource.comments.length }</p>
+        </div>
+    )
+}
+
+export default Resource;
