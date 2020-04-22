d0ab09adcae13095662df8a7a4b6c69f792ea253
David Jones <coreldavid@hotmail.com>
2020-04-21 20:43:25 -0400

    finished modding resource component

--- src/App.js
+++ src/App.js
@@ -1,34 +1,28 @@
 import React, { Component } from 'react';
 import './App.css';
-import Resource from './components/Resource'
+import Resource from './components/Resource';
+import resources from './mock/resources';
 
 class App extends Component {
-  resource = {
-    posterName: 'Cliff',
-    resourceAuthor: 'NetNinja',
-    authorSkillLevel: 'Advanced',
-    cohort: '8',
-    title: 'React & Redux Complete',
-    catergory: ['React', 'Redux'],
-    summary: 'A good resource on building a couple react apps with and without redux',
-    link: 'www.google.come/search?NetNinja%20React',
-    resourceType: 'Playlist',
-    datePublished: '2018-09-10.00:00:00.000z',
-    videoLength: null,
-    timeToComplete: 300,
-    rating: 5,
-    comments: [
-        { user: 'Joe', text: 'Pretty good resource' },
-        { user: 'Phen', text: 'The best to learn reaxt with.' }
-    ]
-}
+  //[<Resource resource={aResource} /> <Resource resource={aResource}]
+  constructor(props){
+    super(props);
+    this.renderPosts.bind(this);
+  }
+
+  renderPosts = () => {
+    const display = resources.map((resource) => {
+      return <Resource resource={resource} />;
+    });
+
+    return display;
+  }
+
   render(){
     return (
       <div className="App">
         <h1>Welcome to BrainHive</h1>
-        <Resource resource={this.resource}/>
-        <Resource resource={this.resource}/>
-        <Resource resource={this.resource}/>
+        {this.renderPosts()}
       </div>
     );
   }
